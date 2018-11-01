9fa34a309385dfce4ef4ede2d537f3aceee250db
Jesus Arrieche <jesusdanielarrieche@gmail.com>
2018-10-31 22:58:24 -0430

    Funciona Ejemplo de Reportes en Excel

--- reporte/reporte.php
+++ reporte/reporte.php
@@ -2,23 +2,17 @@
 
  
     require_once '../MVC/Assets/Librerias/PHPExcel.php';
-    
-        private $driver = "pgsql";
-        private $host = "localhost";
-        private $dbname = "taller";
-        private $user = "postgres";
-        private $password = "jd1234";
-        
-        
-        public function Conectar(){
             
-                $conexion = new PDO("$this->driver:host=$this->host; dbname=$this->dbname", $this->user, $this->password);
+        
+        function Conectar(){
+            
+                $conexion = new PDO("pgsql:host=localhost; dbname=taller", "postgres", "jd1234");
                 $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 
                 return $conexion;
         }
     
-    $consulta = Conexion::Conectar()->prepare("SELECT * FROM clientes");
+    $consulta = Conectar()->prepare("SELECT * FROM clientes");
     $consulta->execute();
     
     $clientes = $consulta->fetchAll(PDO::FETCH_OBJ);
@@ -27,31 +21,31 @@
     
     $excel = new PHPExcel();
     
-    $excel->setProperties()->setCreator("Hidroparts")->setDescription("Reportes de Clientes");
+//    $excel->setProperties()->setCreator("Hidroparts")->setDescription("Reportes de Clientes");
     
     $excel->setActiveSheetIndex(0); // indicamos en la pesta*a donde trabajaremos, iniciando desde el 0 para que sea la primera
-    $excel->setActiveSheet()->setTitle("Clientes"); //Establecemos el titulo
+    $excel->setActiveSheetIndex(0)->setTitle("Clientes"); //Establecemos el titulo
     
     //Definimos Encabezados
     
-    $excel->setActiveSheet()->setCellValue('A1', 'ID');
-    $excel->setActiveSheet()->setCellValue('B1', 'IDENTIFICACION');
-    $excel->setActiveSheet()->setCellValue('C1', 'NOMBRE');
-    $excel->setActiveSheet()->setCellValue('D1', 'APELLIDO');
-    $excel->setActiveSheet()->setCellValue('E1', 'DIRECCION');
-    $excel->setActiveSheet()->setCellValue('F1', 'TELEFONO');
-    $excel->setActiveSheet()->setCellValue('G1', 'CORREO');
+    $excel->setActiveSheetIndex(0)->setCellValue('A1', 'ID');
+    $excel->setActiveSheetIndex(0)->setCellValue('B1', 'IDENTIFICACION');
+    $excel->setActiveSheetIndex(0)->setCellValue('C1', 'NOMBRE');
+    $excel->setActiveSheetIndex(0)->setCellValue('D1', 'APELLIDO');
+    $excel->setActiveSheetIndex(0)->setCellValue('E1', 'DIRECCION');
+    $excel->setActiveSheetIndex(0)->setCellValue('F1', 'TELEFONO');
+    $excel->setActiveSheetIndex(0)->setCellValue('G1', 'CORREO');
     
     //Imprimimos Resultados (Valores)
     
     foreach ( $clientes as $cliente){
-        $excel->setActiveSheet()->setCellValue('A'.$fila , $cliente->id);
-        $excel->setActiveSheet()->setCellValue('B'.$fila , $cliente->identificacion);
-        $excel->setActiveSheet()->setCellValue('C'.$fila , $cliente->nombre);
-        $excel->setActiveSheet()->setCellValue('D'.$fila , $cliente->apellido);
-        $excel->setActiveSheet()->setCellValue('E'.$fila , $cliente->direccion);
-        $excel->setActiveSheet()->setCellValue('F'.$fila , $cliente->telefono);
-        $excel->setActiveSheet()->setCellValue('G'.$fila , $cliente->correo);
+        $excel->setActiveSheetIndex(0)->setCellValue('A'.$fila , $cliente->id);
+        $excel->setActiveSheetIndex(0)->setCellValue('B'.$fila , $cliente->identificacion);
+        $excel->setActiveSheetIndex(0)->setCellValue('C'.$fila , $cliente->nombre);
+        $excel->setActiveSheetIndex(0)->setCellValue('D'.$fila , $cliente->apellido);
+        $excel->setActiveSheetIndex(0)->setCellValue('E'.$fila , $cliente->direccion);
+        $excel->setActiveSheetIndex(0)->setCellValue('F'.$fila , $cliente->telefono);
+        $excel->setActiveSheetIndex(0)->setCellValue('G'.$fila , $cliente->correo);
         
         $fila++;
     }
